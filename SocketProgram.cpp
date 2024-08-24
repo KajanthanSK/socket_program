79d5d5df2abce36ec7f4187c693e5c05190b5547
KajanthanSK <kajanthan@spil.com.au>
2024-08-24 10:23:05 +0530

    set the socket options

--- SocketProgram.cpp
+++ SocketProgram.cpp
@@ -31,5 +31,18 @@ int main()
 		cout << "Socket open successfully"<<endl;
 	}
 
+	/* Set the socket options */
+	int optVal = 1;
+	error = setsockopt(nListenerSocket, SOL_SOCKET, SO_REUSEADDR, (const char*) & optVal, sizeof(optVal));
+
+	if (error < 0)
+	{
+		cout << "Not able to set the socket options" << endl;
+	}
+	else
+	{
+		cout << "Successfully set the socket options" << endl;
+	}
+
 	return 0;
 }
